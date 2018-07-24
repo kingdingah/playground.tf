cc31085091739cfa8a75e4d7f9f7f38105689546
cflynn97 <A00230781@student.ait.ie>
2018-07-24 15:14:21 +0100

    Home page updates + bingo card working

--- js/bingo.js
+++ js/bingo.js
@@ -1,20 +1,32 @@
 $(document).ready(function() {
-    var b_all = ['one','two','three','four','five','six','seven','eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen'];
+    var b_all = ['one','two','three','four','five',
+                'six','seven','eight', 'nine', 'ten',
+                'eleven', 'twelve', 'thirteen', 'fourteen',
+                'fifteen', 'sixteen', 'seventeen', 'eighteen',
+                'nineteen', 'twenty', 'twenty one', 'twenty two',
+                'twenty three','twenty four'];
 
-    for(var i = 0; i < 4; i++)
+    var last = 100;
+    var resb = [];
+    var b_init = b_all.length;
+
+    for(var i = 0; i < 25; i++)
     {
-      var resb = [];
 
-      for(var j = 0; j < 4; j++)
-      {
-        var randb = Math.floor((Math.random() * b_all.length) + 1);
-        delete b_all[randb];
-        console.log(b_all);
-        resb.push(randb);
-      }
+      var randb = Math.floor((Math.random() * b_all.length));
+      console.log(b_all[randb]);
 
-      console.log(resb);
-
-      $(".bingocard").append('<tr><td>' + b_all[resb[0]] + '</td><td>' + b_all[resb[1]] + '</td><td>' + b_all[resb[2]] + '</td><td>' + b_all[resb[3]] + '</td></tr>');
+      resb.push(b_all[randb]);
+      b_all.splice(randb, 1);
     }
+
+    console.log(resb);
+
+    $(".bingocard").append('<tr><td>' + resb[0] + '</td><td>' + resb[1] + '</td><td>' + resb[2] + '</td><td>' + resb[3] + '</td><td>' + resb[4] + '</td></tr>');
+    $(".bingocard").append('<tr><td>' + resb[5] + '</td><td>' + resb[6] + '</td><td>' + resb[7] + '</td><td>' + resb[8] + '</td><td>' + resb[9] + '</td></tr>');
+    $(".bingocard").append('<tr><td>' + resb[10] + '</td><td>' + resb[11] + '</td><td>Free Space</td><td>' + resb[12] + '</td><td>' + resb[13] + '</td></tr>');
+    $(".bingocard").append('<tr><td>' + resb[14] + '</td><td>' + resb[15] + '</td><td>' + resb[16] + '</td><td>' + resb[17] + '</td><td>' + resb[18] + '</td></tr>');
+    $(".bingocard").append('<tr><td>' + resb[19] + '</td><td>' + resb[20] + '</td><td>' + resb[21] + '</td><td>' + resb[22] + '</td><td>' + resb[23] + '</td></tr>');
+    console.log(b_all);
+
 });
--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
+  var bw = $("div.banner").width();
+  var bh = (bw/5)*2;
+  var iw = $("div.quicklinks div").width();
+  console.log(bw + " / " + bh);
 
+  $("div.banner").css({
+    'height':bh
+  });
+
+  $("div.quicklinks div").css({
+    'height':iw
+  });
+
+  $(window).resize(function() {
+    $("div.banner").css({
+      'height':bh
+    });
+
+    $("div.quicklinks div").css({
+      'height':iw
+    });
+  });
 });
